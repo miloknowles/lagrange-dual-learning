f619f17e6ba1c156c8341d88f1b128a223b40ec2
Milo Knowles <mknowles@mit.edu>
2020-04-13 16:35:28 -0400

    Add some failure cases for solver

--- test/test_drake_ik.py
+++ test/test_drake_ik.py
@@ -15,7 +15,8 @@ class DrakeIkTest(unittest.TestCase):
 
     # NOTE: Without an orientation constraint, the solver doesn't find a feasible solution.
     # Need to start near a feasible solution.
-    theta_initial_guess = np.array([-7.72442947, 1.9551931, 7.34003269])
+    theta_initial_guess = np.array([1, 0, 0])
+    # theta_initial_guess = np.array([-7.72442947, 1.9551931, 7.34003269])
 
     theta_value, result = IkThreeLinkMP(theta_initial_guess, x_ee_desired, theta_ee_desired=None)
 
@@ -32,6 +33,26 @@ class DrakeIkTest(unittest.TestCase):
     theta_initial_guess = np.zeros(3)
 
     theta_value, result = IkThreeLinkMP(theta_initial_guess, x_ee_desired, theta_ee_desired=theta_ee_desired)
+
+    print("Solution result: ", result)
+    print("IK solution: ", theta_value)
+    print("EE position: ", ForwardKinematicsThreeLink(theta_value))
+
+    self.viz.DrawRobot(theta_value)
+    self.viz.DrawTarget(x_ee_desired)
+
+  def test_fail(self):
+    x_ee_desired = np.array([1.0, 0.5])
+
+    # NOTE: Without an orientation constraint, the solver doesn't find a feasible solution.
+    # Need to start near a feasible solution.
+    # theta_initial_guess = np.array([0, 0, 0])
+    # theta_initial_guess = np.array([0.1, 0, 0])
+    # theta_initial_guess = np.array([0.2, 0, 0])
+    theta_initial_guess = np.array([0.5, 0, 0])
+
+
+    theta_value, result = IkThreeLinkMP(theta_initial_guess, x_ee_desired, theta_ee_desired=None)
 
     print("Solution result: ", result)
     print("IK solution: ", theta_value)
