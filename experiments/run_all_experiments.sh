89395e2ec752a93a6d25560c2eaeb502b0c25feb
Milo Knowles <mknowles@mit.edu>
2020-04-29 09:37:07 -0400

    Update pretrained models

--- experiments/run_all_experiments.sh
+++ experiments/run_all_experiments.sh
@@ -1,4 +1,4 @@
-./ik_threelink_obs1.sh
-./ik_threelink_obs4.sh
-./ik_threelink_4obs_dynamic.sh
+# ./ik_threelink_obs1.sh
+# ./ik_threelink_obs4.sh
+# ./ik_threelink_4obs_dynamic.sh
 ./ik_threelink_joint_limits_base.sh
